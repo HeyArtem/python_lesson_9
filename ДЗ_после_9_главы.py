735be96bb82760f830731619bfcf24f5b7b28d2a
HeyArtem <artem_white@mail.ru>
2020-09-01 23:05:40 +0300

    ДЗ

--- ДЗ_после_9_главы.py
+++ ДЗ_после_9_главы.py
@@ -76,6 +76,127 @@ class Deck:
         Initializes the Deck with 52 cards.
         '''
         self.cards = []
+        for suit in range(4):
+            for rank in range(1, 10):
+                card = Card(suit, rank)
+                self.cards.append(card)
+    def __str__(self):
+        ''''
+         Returns a string representation of the deck
+         Возвращает строковое представление колоды
+        '''
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+
+    def __add__(self, card):
+        ''''
+        Adds a card to the deck.
+        card: Card
+        '''
+        self.cards.append(card)
+
+    def remove_card(self, card):
+        ''''
+        Removes a card from the deck or raises exception if it is not there.
+        card: Card
+
+        Удаляет карту из колоды или вызывает исключение, если ее нет
+        '''
+        self.cards.remove(card)
+
+    def pop_card(self, i=-1):
+        ''''
+        Removes and returns a card from the deck.
+        i: index of the card to pop; by default, pops the last card.
+
+        Снимает и возвращает карту из колоды.
+         i: индекс карточки, которую нужно открыть; по умолчанию выскакивает последняя карточка.
+
+        '''
+        return self.cards.pop(i)
+
+    def shuffles(self):
+        '''' Shuffles the cards in this deck    '''
+        random.shuffle(self, cards)
+
+    def sort(self):
+        ''''
+        Sorts the cards in ascending order.
+
+        Сортировка карточек по возрастанию.
+        '''
+
+    def move_cards(self, hand, num):
+        ''''
+        Moves the given number of cards from the deck into the Hand.
+        hand: destination Hand object
+        num: integer number of cards to move
+
+        Перемещает заданное количество карт из колоды в руку.
+         рука: пункт назначения Hand объект
+         num: целое число карт для перемещения
+
+        '''
+        for i in range(num):
+            hand.add_card(self.pop_card())
+
+
+
+class Hand(Deck):
+    ''''
+    Represents a hand of playing cards.
+    '''
+    def __init__(self, label=''):
+        self.cards = []
+        self.label = label
+
+    def find_defining_class(obj, method_name):
+        ''''
+        Finds and returns the class object that will provide
+    the definition of method_name (as a string) if it is
+    invoked on obj.
+
+     obj: any python object
+    method_name: string method name
+
+
+
+    Находит и возвращает объект класса, который предоставит
+     определение method_name (в виде строки), если это
+     вызывается на obj.
+
+          obj: любой объект Python
+     method_name: строковое имя метода
+
+
+        '''
+
+        for ty in type(obj).mro():  # что это?
+            if method_name in ty.__dict__:
+                return ty
+        return None
+
+    if __name__ == '__main__':
+        deck = Deck()
+        deck.shuffles()
+
+        hand = Hand()
+        print(find_defining_class(hand, 'shuffle'))
+
+        deck.move_cards(hand, 5)
+        hand.sort()
+        print(hand)
+
+
+
+
+
+
+
+
+
 
 
 
